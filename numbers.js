f1fed770bfa95e2128af769a905ad09984ab106e
ronalynbaniagas <baniagasronalyn@gmail.com>
2024-09-11 20:16:30 +0800

    Add files via upload

---
+++ numbers.js
@@ -0,0 +1,19 @@
+const firstFavNum = 13;
+const secondFavNum = 9;
+const sum = firstFavNum + secondFavNum;
+console.log(sum);
+
+const difference = firstFavNum - secondFavNum;
+console.log(difference);
+
+const product = firstFavNum * secondFavNum;
+console.log(product);
+
+const quotent = firstFavNum / secondFavNum;
+console.log(quotent);
+
+const remainder = firstFavNum % secondFavNum;
+console.log(remainder);
+
+const exponentiation = firstFavNum ** secondFavNum;
+console.log(exponentiation);
